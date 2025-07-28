a062d9a4c28d2fd0faf17701a175f08eea9ae246
Abhijith Ashokan <abhijithashokan43@gmail.com>
2025-07-28 19:13:34 +0530

    subdomain enumeration feature improved. Fixed bugs

--- rf.py
+++ rf.py
@@ -5,44 +5,50 @@ import requests
 import sys
 from urllib.parse import urlparse
 
-payldcnt = 0 
 
 def help():
 	print('''
-	Usage: python3 rf.py domain [options][v-verbose]
+	Usage: DOMAIN [options] [v-verbose]
 	OPTIONS:
 	help - To display this
 	subdomain - Enumerate subdomains
 	dir - Enumerate hidden directories
 	For example
+	   python or python3 rf.py google.com subdomain wordlist.txt
+	   python or python3 rf.py google.com/RF wordlist.txt
 ''')
 def subdomain():
-	try:
-		persis = []
-		print(f"Enumerating Subdomains of {basedomain}\n\n")
-		for subdom in dictionary:
+	payldcnt = 0 
+	persis = []
+	print(f"Enumerating Subdomains of {basedomain}\n")
+	for subdom in dictionary:
+		try:
 			res = requests.get(f'https://{subdom}.{basedomain}')
-			global payldcnt
-			payldcnt += 1
-			print(f'({payldcnt} of {len(dictionary)}): {res.status_code}-->{res.url}')
-	except requests.exceptions.ConnectionError:
-			print(basedomain)
+		except:
+			continue
+		payldcnt += 1
+		print(f'({payldcnt} of {len(dictionary)}) {subdom}: {res.status_code}-->{res.url}')
+		persis.append(subdom)
+
+	print("Subdomain Enumeration Finished",f"\nValid subdomains are -->{persis[0:]}")
 
 def subdir():
-	pass
+	print("Feature not completed yet")
 	
-try:
-	url = urlparse(str(sys.argv[1]))
-	basedomain = url.path
-	print(basedomain)
-	option = str(sys.argv[2])
-	wordlist = str(sys.argv[3])
-		
-	with open(wordlist.strip(),'r')as file:
-		dictionary = file.read().splitlines()
-	 
-
-except IndexError:
-	help
-
-subdomain()
+if len(sys.argv) < 2 or sys.argv[1].lower()=='help':
+	help()
+else:
+	try:
+		url = urlparse(str(sys.argv[2]))
+		basedomain = url.path
+		option = str(sys.argv[1])
+		if option.lower() == "subdomain" or option.lower() == "dir":
+			wordlist = str(sys.argv[3])
+			with open(wordlist.strip(),'r')as file:
+				dictionary = file.read().splitlines()
+			if option.lower() == "subdomain":
+				subdomain()
+			else:
+				subdir()
+	except IndexError:
+		print("Incorrect use of the program\n")
